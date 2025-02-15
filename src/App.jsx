b013a7586bb9e14ea6bddaa98c54b3bac2991487
Anubhav Choubey <choubey.anubhav253@gmail.com>
2025-02-15 10:33:17 +0530

    OK i am bad at CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react'
 import './App.css'
+import StickyInfo from './StickyInfo'
 
 function App() {
   return (
     <>
+      <div className="app">
+        <div className="sticky_column column">
+          <StickyInfo />
+        </div>
+        <div className="content_column column">
+          <h2>Middle Info</h2>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/StickyInfo.jsx
@@ -0,0 +1,10 @@
+import './StickyInfo.css';
+
+export default function StickyInfo() {
+  return (
+    <>
+        <h2>Sticky Info</h2>
+        <p>Sticky info that will always be visible</p>
+    </>
+  )
+}
