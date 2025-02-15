ed3414f43adbeb19cbd6120ccc27e1af9d8391c2
Anubhav Choubey <choubey.anubhav253@gmail.com>
2025-02-15 19:25:05 +0530

    Updated

--- src/StickyInfo.jsx
+++ src/StickyInfo.jsx
@@ -1,7 +1,19 @@
 import "./StickyInfo.css";
 import anubhavImage from "./assets/anubhav.png";
+import anubhavTransparent from "./assets/anubhav_transparent.png";
 import github from "./assets/icons/github.png";
 import linkedin from "./assets/icons/linkedin.png";
+import kaggle from "./assets/icons/kaggle.png";
+import mail from "./assets/icons/mail.png";
+import leetcode from "./assets/icons/leetcode.png";
+
+function IconButton({ href, src, alt }) {
+  return (
+    <a href={href} target="_blank" rel="noopener noreferrer">
+      <img src={src} alt={alt} className="icon" />
+    </a>
+  );
+}
 
 export default function StickyInfo() {
   return (
@@ -14,28 +26,31 @@ export default function StickyInfo() {
           I will learn a way to do it.
         </p>
         <div className="social_links">
-          <a
+          <IconButton
             href="https://github.com/anubhavchoubey"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src=""
-              alt="GitHub"
-              className="icon"
-            />
-          </a>
-          <a
+            src={github}
+            alt="GitHub"
+          />
+          <IconButton
             href="https://www.linkedin.com/in/anubhavchoubey"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://image.flaticon.com/icons/png/512/174/174857.png"
-              alt="LinkedIn"
-              className="icon"
-            />
-          </a>
+            src={linkedin}
+            alt="LinkedIn"
+          />
+          <IconButton
+            href="https://www.linkedin.com/in/anubhavchoubey"
+            src={kaggle}
+            alt="LinkedIn"
+          />
+          <IconButton
+            href="https://www.linkedin.com/in/anubhavchoubey"
+            src={mail}
+            alt="LinkedIn"
+          />
+          <IconButton
+            href="https://www.linkedin.com/in/anubhavchoubey"
+            src={leetcode}
+            alt="LinkedIn"
+          />
         </div>
       </div>
     </div>
